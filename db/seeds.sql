156a817ab4dc775098e724ead8db5b0ac1329a06
Travis Hill <travismhill@gmail.com>
2020-10-08 18:05:02 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('JR Bacon Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Swiss Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Plain Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name) VALUES ('Dbl Cheeseburger');
